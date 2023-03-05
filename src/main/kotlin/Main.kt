3a5d4fb50d12bf55110d4ffccfb48a0fd046ea78
Danijel <danijel.sojat@gmail.com>
2023-03-05 21:08:44 +0100

    novi zeko commitan

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,49 @@
+fun main(args: Array<String>) {
+
+    val r01 = "     / \\"
+    val r02 = "    /   \\"
+    val r03 = "   |  _  |"
+    val r04 = "   | / \\ |"
+    val r05 = "   ||   ||    ______"
+    val r06 = "   ||   ||   |\\     \\"
+    val r07 = "   ||   ||   ||\\     \\"
+    val r08 = "   ||   ||   || \\    |"
+    val r09 = "   ||   ||   ||  \\__/"
+    val r10 = "   ||   ||   ||   ||"
+    val r11 = "    \\\\_/  \\_/  \\_//"
+    val r12 = "   /   _       _   \\"
+    val r13 = "  /                 \\"
+    val r14 = "  |    o       o    |"
+    val r15 = "  |   \\   ___   /   |"
+    val r16 = "  /    \\  \\_/  /    \\"
+    val r17 = " /  ----   |   --\\   \\"
+    val r18 = " |     \\__/|\\__/  \\  |"
+    val r19 = " \\       |_|_|       /"
+    val r20 = "  \\_____       _____/"
+    val r21 = "        \\     /"
+    val r22 = "        |     |"
+
+
+    println(r01)
+    println(r02)
+    println(r03)
+    println(r04)
+    println(r05)
+    println(r06)
+    println(r07)
+    println(r08)
+    println(r09)
+    println(r10)
+    println(r11)
+    println(r12)
+    println(r13)
+    println(r14)
+    println(r15)
+    println(r16)
+    println(r17)
+    println(r18)
+    println(r19)
+    println(r20)
+    println(r21)
+    println(r22)
+}
